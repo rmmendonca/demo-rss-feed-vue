116f08fb6bfe175a598b7fc78a21ef7f2007761e
Ricardo M Mendonça <ricmm89@gmail.com>
2021-01-09 13:06:17 -0300

    inicial

--- README.md
+++ README.md
@@ -17,3 +17,24 @@ npm run build
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+http://feed43.com/feed.html?name=4502845176441015
+
+
+RUNKIT:
+https://npm.runkit.com/rss-parser
+
+let Parser = require('rss-parser');
+let parser = new Parser();
+
+(async () => {
+
+  let feed = await parser.parseURL('http://feed43.com/4502845176441015.xml');
+  console.log(feed.title);
+
+  feed.items.forEach(item => {
+    console.log(item)
+  });
+
+})();
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img alt="Vue logo" src="../assets/logo.png" />
+		{{ this.feed }}
+	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+
+	data() {
+		return {
+			feed: null,
+		};
+	},
+
+	components: {
+		HelloWorld,
+	},
+	created() {
+		let Parser = require("rss-parser");
+
+		const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
+
+		let parser = new Parser();
+		parser.parseURL(
+			CORS_PROXY + "http://feed43.com/4502845176441015.xml"
+		).then((f) => {
+      console.log(f.title);
+				f.items.forEach(function(entry) {
+          console.log(entry.title + ":" + entry.link);
+				});
+        this.feed = f;
+    }).catch((error) => {
+      console.log(error);
+    });
+	},
+};
 </script>
